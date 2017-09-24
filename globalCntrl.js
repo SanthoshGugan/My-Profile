a7559aca3c8f5108817f36b7bc37cf8a45fd85cb
Santhosh Gugan <santhoshg1990@gmail.com>
2017-09-24 17:57:05 +0530

    UI router implementation

--- globalCntrl.js
+++ globalCntrl.js
@@ -1,4 +1,17 @@
-var globalMod = angular.module("globalMod",[]);
+var globalMod = angular.module("globalMod",['ui.router']);
+
+/*Configuration for router*/
+globalMod.config(['$stateProvider','$urlRouteProvider',function($stateProvider,$urlRouteProvider) {
+
+	$urlRouteProvider.otherwise('aboutMe');
+	var aboutMeState= {
+		name: 'aboutMe',
+		url: 'about-me',
+		templateUrl: './modules/aboutMe/aboutMe.html',
+		controller: './modules/aboutMe/aboutMeCntrl'
+	};
+	$stateProvider.state(aboutMeState);
+}]);
 
 globalMod.controller('globalCntrl', [function(){
 	var vm = this;
