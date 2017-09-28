5defe46d35ee5e0432039828337da115d262b75c
Santhosh Gugan <santhoshg1990@gmail.com>
2017-09-28 20:56:24 +0530

    Social Icons

--- modules/aboutMe/aboutMeCntrl.js
+++ modules/aboutMe/aboutMeCntrl.js
@@ -1,3 +1,12 @@
 globalMod.controller('aboutMeCntrl', [function(){
-	
+	var vm = this;
+	vm.myAge =	_calculateAge(1990,10,05);
+
+	console.log(vm.myAge);
+
+	function _calculateAge(year, mm,dd) { // birthday is a date
+		var bd  = new Date(year,mm,dd);
+	    var today = new Date();
+	    return Math.abs(today.getUTCFullYear() - bd.getUTCFullYear());
+	};
 }])
